867233095fc2d2d1aba31a96a272ea5e354fbbe4
Manoj Kumar Gupta <manoj14jan@gmail.com>
2020-03-30 22:06:39 -0500

    1

--- W1D4/script/script.js
+++ W1D4/script/script.js
@@ -4,3 +4,18 @@ function showLoginFields() {
     console.log("Password: " + document.getElementById("exampleInputPassword1").value);
     return false;
 }
+
+function newProductFields() {
+    console.log("test");
+    try {
+        alert("Product Number: " + document.getElementById("product-number").value +"\n"
+        + "Product Name: " + document.getElementById("name").value+"\n"
+        +"Unit Price: " + document.getElementById("unit-price").value+"\n"
+        + "Quntity in Stock: " + document.getElementById("quntity-in-stock").value+"\n"
+        + "Supplier: " + document.getElementById("supplier").value+"\n"
+        + "Date Supplied: " + document.getElementById("date-supplied").value);
+    } catch (error) {
+        alert(error)
+    }
+     return false;
+}
